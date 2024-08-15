f304bd59fee2d0cd8d0f2e90a130d88944a1b443
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-08-15 16:11:26 +0200

    Add reference

--- parliamentarch.js
+++ parliamentarch.js
@@ -1,4 +1,9 @@
 "use strict";
 
+/**
+ * See the Python Parliamentarch module for documentation.
+ * https://github.com/Gouvernathor/parliamentarch
+ */
+
 import { get_nrows_from_nseats, get_row_thickness, get_seats_centers } from "parliamentarch/geometry.js";
 export { SeatData } from "parliamentarch/svg.js";
